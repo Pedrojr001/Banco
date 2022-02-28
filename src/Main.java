45b4d467ca2ff11dc832513d4605cdc15e60b29d
Pedrojr001 <pedrojr001@gmail.com>
2022-02-28 08:37:40 -0300

    Expert Talk

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Conta cc =  new ContaCorrente();
+		Conta poupanca =  new ContaPoupanca();
+		
+		cc.imprimirInfosComuns();
+		poupanca.imprimirInfosComuns();
+	}
+
+}
